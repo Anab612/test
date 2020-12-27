425d3b3b284415aec09b87638e7dc65d9b9c4597
Anab612 <twins.b612@gmail.com>
2020-12-27 14:31:39 -0300

    nova versão no principal.py, agora ele diz se o numero é par ou ímpar

--- principal.py
+++ principal.py
@@ -1,1 +1,10 @@
 print("oiii")
+
+numero = int(input("Digite um numero: "))
+
+ehPar = numero % 2 == 0
+
+if ehPar:
+    print("{} eh par".format(numero))
+else:
+    print("{} é ímpar".format(numero))
